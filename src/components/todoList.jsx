4165fd29545dfcd2cbf0cc578d2a1a538cee382f
silanurs <sarii.silanur@gmail.com>
2024-02-03 22:30:03 +0300

    format date

--- src/components/todoList.jsx
+++ src/components/todoList.jsx
@@ -1,20 +1,12 @@
-const List = () => {
-const todos=[{id:1, description:"learn contextAPI"}]
+const List = ({todos}) => {
+
   return (
       <article>
           {todos.map((todo) => {
               return <div key={todo.id} id="list">
               <input type="checkbox" className="checkbox"/>      
               <div>{todo.description}</div>
-              <select id="colors" style={{width:"100px"}}>
-                  <option value=""></option>
-                  <option value="green" style={{color:"green"}}>Green</option>
-                  <option value="blue" style={{color:"blue"}}>Blue</option>
-                  <option value="orange" style={{color:"orange"}}>Orange</option>
-                  <option value="yellow" style={{color:"yellow"}}>Yellow</option>
-                  <option value="purple" style={{color:"purple"}}>Purple</option>
-                  <option value="red" style={{color:"red"}}>Red</option>
-              </select>
+              <div>{todo.date}</div>
               <div className="delete">&#215;</div>
               
               </div>
--- src/components/todos.jsx
+++ src/components/todos.jsx
@@ -1,18 +1,36 @@
 import List from "./todoList"
 import Menu from "./menu"
+import { useState } from "react"
+import dateFormat, {masks} from "dateformat";
 
 const Todos = () =>{
+    const [todos, setTodos] = useState([{id:1, description:"learn contextAPI"}]);
+    const [info, setInfo] = useState({});
+    const handleChange = (e)=>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setInfo(inputs => ({...inputs, [name]:value}));
+        setInfo(inputs=>({...inputs, date:dateFormat(date,"paddedShortDate")}))
+        
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        setTodos([...todos, info])
+        setInfo(inputs=>({...inputs, description:"", date:""}))
+    }
  
     return (
         <main>   
             <Menu/> 
             <div id="todo-container">
-            <form > 
-                <input type="text" id="description" placeholder="What needs to be done?" required ></input>
+            <form> 
+                <input type="text" id="description" placeholder="What needs to be done?" required name="description" value={info.description} onChange={handleChange}></input>
                 <label htmlFor="description"></label>  
+                <input type="date" id="date"required name="date" value={info.date} onChange={handleChange} data-date="" data-date-format="DD MMMM YYYY"></input>
             </form>
+            <button type="submit" onClick={handleSubmit}>Submit</button>
             <hr></hr>
-            <List/>
+            <List todos={todos}/>
             </div>          
           
         </main>
