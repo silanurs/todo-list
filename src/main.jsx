619fc78ee0faafd726f83dd03c9f796fdf6d0f23
silanur <sarii.silanur@gmail.com>
2024-02-20 14:07:29 +0300

    routing using createBrowserRouter instead of BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,44 @@
-
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import Todos from './components/todos';
+import Today from './components/today';
+import Year from './components/year';
+import Notes from './components/notes';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+const router = createBrowserRouter([
+
+  {
+    path:"/todo-list/",
+    element:<App></App>,
+    children:[
+      {
+        index:true,
+        path:"Home",
+        element:<Todos></Todos>
+      },
+      {
+        path:"Today",
+        element:<Today></Today>
+      },
+      {
+        path:"Year", 
+        element:<Year></Year>
+      },
+      {
+        path:"Notes",
+        element:<Notes></Notes>
+      }
+    ]
+  },
+
+])
 
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   //didn't use <React.StrictMode> because it was rendering the page twice on mount
-    <BrowserRouter>
-       <App />
-    </BrowserRouter>
+   <RouterProvider router={router}></RouterProvider>
 
 );
 
